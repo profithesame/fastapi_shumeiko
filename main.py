dcc9351b5881fba16d0c025b8597e77bbcb9d470
Vladimir <vladimir@datadrivemd.com>
2024-04-23 16:29:39 +0300

    SD-1 feat(first): first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
+from database import create_tables, delete_tables
+from router import router as tasks_router
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await delete_tables()
+    print("DB dropped")
+    await create_tables()
+    print("DB init")
+    yield
+    print("Closing...")
+    
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(router=tasks_router)
+
